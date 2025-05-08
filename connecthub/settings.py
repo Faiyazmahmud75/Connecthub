d8be8e4de6e0267d7d5b3c275a2a5d17226b3ec9
faiyazmahmud75 <faiyazmahmud75@gmail.com>
2025-05-08 06:22:45 +0600

    updated settings.py

--- connecthub/settings.py
+++ connecthub/settings.py
@@ -137,7 +137,11 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+       "staticfiles": {
+           "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+       }
+   }
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
