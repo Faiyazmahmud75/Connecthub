bbdd6d0ddfcceb9123b081363726b992af2359c1
Faiyaz Mahmud Toha <67873766+Faiyazmahmud75@users.noreply.github.com>
2025-05-08 14:21:24 +0600

    removed ssl certificate requirement

--- connecthub/settings.py
+++ connecthub/settings.py
@@ -89,7 +89,7 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
